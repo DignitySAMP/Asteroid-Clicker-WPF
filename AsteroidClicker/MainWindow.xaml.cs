840dc5bfdd5af69f2d6c2649a00da995c0ca5635
Davy Lijcops <davy.lijcops@student.pxl.be>
2024-01-06 03:08:20 +0100

    COOKIE CLICKER-32
    - Animatie was reeds voltooid in COOKIE CLICKER-04, maar is nu naar onderkant van de codespace verplaatst voor volgorde van draaiboek te representeren

--- AsteroidClicker/MainWindow.xaml.cs
+++ AsteroidClicker/MainWindow.xaml.cs
@@ -824,128 +824,6 @@ namespace AsteroidClicker
         }
 
         #endregion
-        #region Particle System (Blast/Debris)
-        // Blast Effect
-        string[] blastParticleImages =
-        {
-            "/assets/particles/blast/click_blast_0.png", "/assets/particles/blast/click_blast_1.png", "/assets/particles/blast/click_blast_2.png",
-            "/assets/particles/blast/click_blast_3.png", "/assets/particles/blast/click_blast_4.png", "/assets/particles/blast/click_blast_5.png",
-            "/assets/particles/blast/click_blast_6.png", "/assets/particles/blast/click_blast_7.png", "/assets/particles/blast/click_blast_8.png",
-            "/assets/particles/blast/click_blast_9.png"
-        };
-
-        Image blastParticleImg = new Image();
-        DispatcherTimer blast_timer = new DispatcherTimer();
-        int blastCurrentImage = 0;
-
-        private void CreateBlastParticle()
-        {
-            ClearBlastParticle();
-
-            blastParticleImg = new Image();
-            blastParticleImg.Source = new BitmapImage(new Uri(blastParticleImages[0], UriKind.Relative));
-
-            blastParticleImg.Width = 96;
-            blastParticleImg.Height = 96;
-            blastParticleImg.IsHitTestVisible = false; // can click through image
-
-            blastCurrentImage = 0;
-
-
-            PlaySyncedSound(Properties.Resources.blaster);
-
-            GridBlastZone.Children.Add(blastParticleImg);
-
-            blast_timer.Start();
-        }
-        private void Blast_Timer(object sender, EventArgs e)
-        {
-            blastCurrentImage++;
-            if (blastCurrentImage >= blastParticleImages.Length)
-            {
-                blastCurrentImage = 0;
-                GridBlastZone.Children.Remove(blastParticleImg);
-                blast_timer.Stop();
-            }
-            else blastParticleImg.Source = new BitmapImage(new Uri(blastParticleImages[blastCurrentImage], UriKind.Relative));
-        }
-
-        private void ClearBlastParticle()
-        {
-            blastCurrentImage = 0;
-            GridBlastZone.Children.Remove(blastParticleImg);
-            blast_timer.Stop();
-        }
-
-        // Dropping debris/particle effecft
-        string[] fallingParticleImages =
-        {
-            "/assets/particles/drop/click_anim_0.png", "/assets/particles/drop/click_anim_1.png", "/assets/particles/drop/click_anim_2.png",
-            "/assets/particles/drop/click_anim_3.png", "/assets/particles/drop/click_anim_4.png"
-        };
-
-        private static int MAX_FALLING_PARTICLES = 15;
-        List<int> fallingParticleIndexes = new List<int>();
-        Image[] fallingParticleImg = new Image[MAX_FALLING_PARTICLES];
-        private void CreateFallingParticles()
-        {
-            ClearFallingParticles();
-
-            Random random = new Random();
-            int MAX_SPAWNED_PARTICLES = random.Next(5, MAX_FALLING_PARTICLES);
-
-            for (int index = 0; index < MAX_SPAWNED_PARTICLES; index++)
-            {
-                // Storing random sprite
-                fallingParticleImg[index] = new Image();
-                fallingParticleImg[index].Source = new BitmapImage(new Uri(fallingParticleImages[random.Next(fallingParticleImages.Length)], UriKind.Relative));
-
-                // Set random size
-                fallingParticleImg[index].Width = random.Next(12, 32);
-                fallingParticleImg[index].Height = random.Next(0, 32);
-
-                // Setting random position
-                double pos_x = random.Next(0, (int)CanvasDropParticles.Width - 20);
-                double pos_y = random.Next(-32, -16);
-
-                Canvas.SetLeft(fallingParticleImg[index], pos_x);
-                Canvas.SetTop(fallingParticleImg[index], pos_y);
-
-                // Showing inside particle canvas & adding to list
-                CanvasDropParticles.Children.Add(fallingParticleImg[index]);
-                fallingParticleIndexes.Add(index);
-            }
-        }
-        private void MoveFallingParticles()
-        {
-            Random random = new Random();
-            foreach (int index in fallingParticleIndexes)
-            {
-                double pos_y = Canvas.GetTop(fallingParticleImg[index]);
-
-                pos_y += random.Next(1, 5);
-                fallingParticleImg[index].Opacity -= 0.035;
-
-                Canvas.SetTop(fallingParticleImg[index], pos_y);
-
-                if (pos_y >= CanvasDropParticles.Height)
-                {
-                    CanvasDropParticles.Children.Remove(fallingParticleImg[index]);
-                    fallingParticleIndexes.Remove(index);
-                    break;
-                }
-            }
-        }
-
-        private void ClearFallingParticles()
-        {
-            foreach (int index in fallingParticleIndexes)
-            {
-                CanvasDropParticles.Children.Remove(fallingParticleImg[index]);
-            }
-            fallingParticleIndexes.Clear();
-        }
-        #endregion
         #region Custom Message System
 
         private async void ShowFadeMessage(string title, string text)
@@ -1695,7 +1573,6 @@ namespace AsteroidClicker
             }
         }
         #endregion
-
         #region Muting Sound
         bool IsSoundMuted = false;
 
@@ -1735,6 +1612,128 @@ namespace AsteroidClicker
             }
         }
 
+        #endregion
+        #region Particle System (Blast/Debris)
+        // Blast Effect
+        string[] blastParticleImages =
+        {
+            "/assets/particles/blast/click_blast_0.png", "/assets/particles/blast/click_blast_1.png", "/assets/particles/blast/click_blast_2.png",
+            "/assets/particles/blast/click_blast_3.png", "/assets/particles/blast/click_blast_4.png", "/assets/particles/blast/click_blast_5.png",
+            "/assets/particles/blast/click_blast_6.png", "/assets/particles/blast/click_blast_7.png", "/assets/particles/blast/click_blast_8.png",
+            "/assets/particles/blast/click_blast_9.png"
+        };
+
+        Image blastParticleImg = new Image();
+        DispatcherTimer blast_timer = new DispatcherTimer();
+        int blastCurrentImage = 0;
+
+        private void CreateBlastParticle()
+        {
+            ClearBlastParticle();
+
+            blastParticleImg = new Image();
+            blastParticleImg.Source = new BitmapImage(new Uri(blastParticleImages[0], UriKind.Relative));
+
+            blastParticleImg.Width = 96;
+            blastParticleImg.Height = 96;
+            blastParticleImg.IsHitTestVisible = false; // can click through image
+
+            blastCurrentImage = 0;
+
+
+            PlaySyncedSound(Properties.Resources.blaster);
+
+            GridBlastZone.Children.Add(blastParticleImg);
+
+            blast_timer.Start();
+        }
+        private void Blast_Timer(object sender, EventArgs e)
+        {
+            blastCurrentImage++;
+            if (blastCurrentImage >= blastParticleImages.Length)
+            {
+                blastCurrentImage = 0;
+                GridBlastZone.Children.Remove(blastParticleImg);
+                blast_timer.Stop();
+            }
+            else blastParticleImg.Source = new BitmapImage(new Uri(blastParticleImages[blastCurrentImage], UriKind.Relative));
+        }
+
+        private void ClearBlastParticle()
+        {
+            blastCurrentImage = 0;
+            GridBlastZone.Children.Remove(blastParticleImg);
+            blast_timer.Stop();
+        }
+
+        // Dropping debris/particle effecft
+        string[] fallingParticleImages =
+        {
+            "/assets/particles/drop/click_anim_0.png", "/assets/particles/drop/click_anim_1.png", "/assets/particles/drop/click_anim_2.png",
+            "/assets/particles/drop/click_anim_3.png", "/assets/particles/drop/click_anim_4.png"
+        };
+
+        private static int MAX_FALLING_PARTICLES = 15;
+        List<int> fallingParticleIndexes = new List<int>();
+        Image[] fallingParticleImg = new Image[MAX_FALLING_PARTICLES];
+        private void CreateFallingParticles()
+        {
+            ClearFallingParticles();
+
+            Random random = new Random();
+            int MAX_SPAWNED_PARTICLES = random.Next(5, MAX_FALLING_PARTICLES);
+
+            for (int index = 0; index < MAX_SPAWNED_PARTICLES; index++)
+            {
+                // Storing random sprite
+                fallingParticleImg[index] = new Image();
+                fallingParticleImg[index].Source = new BitmapImage(new Uri(fallingParticleImages[random.Next(fallingParticleImages.Length)], UriKind.Relative));
+
+                // Set random size
+                fallingParticleImg[index].Width = random.Next(12, 32);
+                fallingParticleImg[index].Height = random.Next(0, 32);
+
+                // Setting random position
+                double pos_x = random.Next(0, (int)CanvasDropParticles.Width - 20);
+                double pos_y = random.Next(-32, -16);
+
+                Canvas.SetLeft(fallingParticleImg[index], pos_x);
+                Canvas.SetTop(fallingParticleImg[index], pos_y);
+
+                // Showing inside particle canvas & adding to list
+                CanvasDropParticles.Children.Add(fallingParticleImg[index]);
+                fallingParticleIndexes.Add(index);
+            }
+        }
+        private void MoveFallingParticles()
+        {
+            Random random = new Random();
+            foreach (int index in fallingParticleIndexes)
+            {
+                double pos_y = Canvas.GetTop(fallingParticleImg[index]);
+
+                pos_y += random.Next(1, 5);
+                fallingParticleImg[index].Opacity -= 0.035;
+
+                Canvas.SetTop(fallingParticleImg[index], pos_y);
+
+                if (pos_y >= CanvasDropParticles.Height)
+                {
+                    CanvasDropParticles.Children.Remove(fallingParticleImg[index]);
+                    fallingParticleIndexes.Remove(index);
+                    break;
+                }
+            }
+        }
+
+        private void ClearFallingParticles()
+        {
+            foreach (int index in fallingParticleIndexes)
+            {
+                CanvasDropParticles.Children.Remove(fallingParticleImg[index]);
+            }
+            fallingParticleIndexes.Clear();
+        }
         #endregion
     }
 }
